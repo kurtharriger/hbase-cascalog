1093cf63be6b2ccdd082ceffcb643d95f4caf3e9
Kurt Harriger <kurtharriger@gmail.com>
2012-07-13 10:33:41 -0600

    update to lein2 project. update cascalog to 1.8.7 (1.9.0 has breaking changes)

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject hbase.cascalog "0.1-SNAPSHOT"
-  :source-path "src/clj"
+  :source-paths ["src/clj"]
   :java-fork "true"
   :java-debug "true"
   :description "simple cascalog wrapper for cascading.hbase"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.kurtharriger/cascading.hbase "1.2.8-SNAPSHOT"]
-                 [cascalog "1.8.2"]
+                 [cascalog "1.8.7"]
                  [log4j/log4j "1.2.15" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [commons-logging/commons-logging "1.0.4"]]
-  :dev-dependencies [[lein-hadoop "1.0.0"]]
+  :dev-dependencies [[org.clojars.kurtharriger/lein-hadoop "1.0.1"]]
   :jvm-opts ["-Xmx1024m" "-server"]
   :main hbase.cascalog.sample
   :aot :all)
